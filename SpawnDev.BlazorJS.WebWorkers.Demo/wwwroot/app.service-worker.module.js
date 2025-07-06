f2f495ea430676afaa2a40908f3da5c33d27244f
Todd Tanner <todd@spawndev.com>
2025-07-06 00:34:21 -0400

    Set correct globals file

--- SpawnDev.BlazorJS.WebWorkers.Demo/wwwroot/app.service-worker.module.js
+++ SpawnDev.BlazorJS.WebWorkers.Demo/wwwroot/app.service-worker.module.js
@@ -1,7 +1,7 @@
 ﻿// A custom service worker or web worker startup script can be used to static import Javascript libraries.
 // This was specifically tested to allow Blazor WASM to run in a Chrome Browser extension background ServiceWorker running in 'module' mode so it can run Transformers.js
 
-import * as _globals from "./app.worker.module.globals.js"
+import * as _globals from "./app.service-worker.module.globals.js"
 import * as _importShim from "./spawndev.blazorjs.webworkers.module.js"
 
 // Start Blazor
